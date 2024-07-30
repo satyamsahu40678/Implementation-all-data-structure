cd4e7b87423a5d30972f62f0f4c19af7aed71090
nerdnightboy <nerdnightboy@gmail.com>
2024-07-30 22:50:07 +0530

    Queue using array implemented

---
+++ QueueUsingStack.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Implement{
+    Stack<Integer> st1;
+    Stack<Integer> st2;
+
+    public Implement(){
+        st1 = new Stack<>();
+        st2 = new Stack<>();
+    }
+
+    public void enqueue(int val){
+        st1.push(val);
+    }
+
+    public boolean isEmpty(){
+        return st1.isEmpty() && st2.isEmpty();
+    }
+
+    public int dequeue(){
+        if(st2.isEmpty()){
+            while(!st1.isEmpty()){
+                st2.push(st1.pop());
+            }
+        }
+        if(st2.isEmpty()){
+            return -1;
+        }
+        return st2.pop();
+    }
+
+}
+
+public class QueueUsingStack {
+    public static void main(String[] args) {
+        Implement im = new Implement();
+        im.enqueue(10);
+        im.enqueue(20);
+        im.enqueue(30);
+        System.out.println(im.dequeue());
+        System.out.println(im.dequeue());
+        System.out.println(im.dequeue());
+        System.out.println(im.isEmpty());
+    }
+
+}
